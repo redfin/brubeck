1c2534b8b3edbb7c3ac6a1b2a08526bb35f5f5b4
Jason Lunz <jason.lunz@redfin.com>
2015-12-02 15:56:28 -0800

    add unit tests for valid and invalid sample rates

--- tests/statsd_msg.c
+++ tests/statsd_msg.c
@@ -3,22 +3,26 @@
 #include "sput.h"
 #include "brubeck.h"
 
-static void try_parse(struct brubeck_statsd_msg *msg, const char *msg_text, double expected)
+static void try_parse(struct brubeck_statsd_msg *msg, const char *msg_text, int expect_parse, double expected)
 {
 	char buffer[64];
 	strcpy(buffer, msg_text);
 
-	sput_fail_unless(brubeck_statsd_msg_parse(msg, buffer) == 0, msg_text);
-	sput_fail_unless(expected == msg->value.n, "msg.value.n == expected");
+	sput_fail_unless(brubeck_statsd_msg_parse(msg, buffer) == expect_parse, msg_text);
+	if(0 == expect_parse) {
+		sput_fail_unless(expected == msg->value.n, "msg.value.n == expected");
+	}
 }
 
-static void try_parse_set(struct brubeck_statsd_msg *msg, const char *msg_text, const char *expected)
+static void try_parse_set(struct brubeck_statsd_msg *msg, const char *msg_text, int expect_parse, const char *expected)
 {
 	char buffer[64];
 	strcpy(buffer, msg_text);
 
-	sput_fail_unless(brubeck_statsd_msg_parse(msg, buffer) == 0, msg_text);
-	sput_fail_unless(0 == strcmp(msg->value.s, expected), "msg.value.s == expected");
+	sput_fail_unless(brubeck_statsd_msg_parse(msg, buffer) == expect_parse, msg_text);
+	if(0 == expect_parse) {
+		sput_fail_unless(0 == strcmp(msg->value.s, expected), "msg.value.s == expected");
+	}
 }
 
 static void try_split(struct brubeck_sampler *sampler, const char *input, int expected)
@@ -32,16 +36,19 @@ static void try_split(struct brubeck_sampler *sampler, const char *input, int ex
 			"brubeck_statsd_split_buffer() == expected");
 }
 
-static struct { char *metric; double expected; } test_values[] = {
-	{ "github.auth.fingerprint.sha1:1|c", 1 },
-	{ "github.auth.fingerprint.sha1:1|c|@args", 1 },
-	{ "github.auth.fingerprint.sha1:1|g", 1 },
-	{ "lol:1|ms", 1 },
-	{ "this.is.sparta:199812|C", 199812 },
-	{ "this.is.sparta:0012|h", 12 },
-	{ "this.is.sparta:23.23|g", 23.23 },
-	{ "this.is.sparta:0.232030|g", 0.23203 },
-	{ "this.are.some.floats:1234567.89|g", 1234567.89 },
+static struct { char *metric; int parse; double expected; } test_values[] = {
+	{ "github.auth.fingerprint.sha1:1|c", 0, 1 },
+	{ "github.auth.fingerprint.sha1:1|c|@args", -1, 1 },
+	{ "github.auth.fingerprint.sha1:1|g", 0, 1 },
+	{ "lol:1|ms", 0, 1 },
+	{ "lol.b:2|ms@0.1", 0, 2 },
+	{ "lol.b:2|ms@-0.1", -1, 2 },
+	{ "lol.b:2|ms@1.14", -1, 2 },
+	{ "this.is.sparta:199812|C", 0, 199812 },
+	{ "this.is.sparta:0012|h", 0, 12 },
+	{ "this.is.sparta:23.23|g", 0, 23.23 },
+	{ "this.is.sparta:0.232030|g", 0, 0.23203 },
+	{ "this.are.some.floats:1234567.89|g", 0, 1234567.89 },
 	{ NULL, 0 },
 };
 
@@ -51,12 +58,12 @@ void test_statsd_msg__parse_strings(void)
 	int i;
 
 	for(i = 0; test_values[i].metric != NULL; i++) {
-		try_parse(&msg, test_values[i].metric, test_values[i].expected);
+		try_parse(&msg, test_values[i].metric, test_values[i].parse, test_values[i].expected);
 	}
 
 #define EXPECT "some.non-float.value"
-	try_parse_set(&msg, "this.is.a.set:" EXPECT "|s|@args", EXPECT);
-	try_parse_set(&msg, "this.is.a.set:" EXPECT "|s", EXPECT);
+	try_parse_set(&msg, "this.is.a.set:" EXPECT "|s|@args", -1, EXPECT);
+	try_parse_set(&msg, "this.is.a.set:" EXPECT "|s", 0, EXPECT);
 }
 
 void test_statsd_msg__split_buffer(void)
@@ -71,7 +78,7 @@ void test_statsd_msg__split_buffer(void)
 	sampler.type = BRUBECK_SAMPLER_STATSD;
 
 	for(i = 0; test_values[i].metric != NULL; i++) {
-		try_split(&sampler, test_values[i].metric, 1);
+		try_split(&sampler, test_values[i].metric, test_values[i].parse == 0 ? 1 : 0);
 	}
 
 	try_split(&sampler,
@@ -86,6 +93,7 @@ void test_statsd_msg__split_buffer(void)
 	try_split(&sampler,
 		"missing.metric.type:0@trail\n"
 		"ab.cd.ef:0|g@foo\n"
+		"gh.ij.k:0|C@0.6\n"
 		"missing.metric.value|g\n"
 		"x.y.z:-1|g\n"
 		, 2);
